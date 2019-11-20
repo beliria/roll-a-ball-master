2caa8bd0b07d5b9aac76f023e2261ac56f314a45
ethhhereal <57172115+ethhhereal@users.noreply.github.com>
2019-11-20 10:50:37 +0100

    optimization

---
+++ Assets/Scripts/DeathZone.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+    public Transform Spawn;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.transform.position = Spawn.position;
+        }
+    }
+}
